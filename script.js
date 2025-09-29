14f5688ad0d0375a9c2ee4eb155b595d25da4de9
adjeisilas <adjeisikapasilas@gmail.com>
2025-09-29 10:50:48 -0700

    Added a transition in the imgbox if an error occurs and also added a condition in the js file

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ let qrImage = document.getElementById("qrimage");
 let qrText = document.getElementById("qrtext");
 
 function generateQR() {
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
     imgBox.classList.add("show-img");
+  }
+  else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+}
 }
