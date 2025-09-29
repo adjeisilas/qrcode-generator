ca49b4f3e6400ba0b28d008b362c13fc9f8d7dd6
adjeisilas <adjeisikapasilas@gmail.com>
2025-09-29 10:25:05 -0700

    wrote the js scripts, and the function in the index file and also add the script file in the index file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let imgBox = document.getElementById("imgbox");
+let qrImage = document.getElementById("qrimage");
+let qrText = document.getElementById("qrtext");
+
 function generateQR() {
-    const input = document.querySelector('input').value;
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 }
